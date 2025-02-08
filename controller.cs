6d14a5636c03af3e4499cfb28dcf0ee41fd4fa3b
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2025-02-08 15:12:00 -0500

    Add files via upload

---
+++ controller.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class controller : MonoBehaviour
+{
+    public Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+    public float moveLimiter = 0.7f;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0)
+        {
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
